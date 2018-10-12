2239d9bf19cb06ede92adc921f287062b6d4476d
ydarma <Ba1eevHe>
2018-10-12 18:53:10 +0200

    SlidingPuzzle arguments parsing

--- src/MainArgs.hs
+++ src/MainArgs.hs
@@ -8,3 +8,11 @@ parseBowlingGameArgs :: [String] -> BowlingGameArgs
 parseBowlingGameArgs args =
   let rolls = map read args
    in BowlingGameArgs {rolls = rolls}
+
+newtype SlidingPuzzleArgs = SlidingPuzzleArgs
+  { moves :: Int
+  }
+
+parseSlidingPuzzleArgs :: [String] -> SlidingPuzzleArgs
+parseSlidingPuzzleArgs [moves] = SlidingPuzzleArgs {moves = read moves}
+parseSlidingPuzzleArgs _       = error "trailing arguments"
--- test/MainSpec.hs
+++ test/MainSpec.hs
@@ -1,11 +1,35 @@
 module MainSpec where
 
+import           Control.Exception
 import           MainArgs
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
 test_parseBowlingGameArgs :: TestTree
 test_parseBowlingGameArgs =
-  let args = parseBowlingGameArgs (["3", "7", "8"])
-   in testCase "Testing parse BowlingGame arguments" $ do
-        (assertEqual "Args should be 3 7 8" [3, 7, 8] (rolls args))
+  let args = parseBowlingGameArgs ["3", "7", "8"]
+   in testCase "Testing parse BowlingGame arguments" $
+      assertEqual "Args should be 3 7 8" [3, 7, 8] (rolls args)
+
+test_parseSlidingPuzzleArgs :: TestTree
+test_parseSlidingPuzzleArgs =
+  let args = parseSlidingPuzzleArgs ["100"]
+   in testCase "Testing parse SlidingPuzzle arguments" $
+      assertEqual "Args should be 100" 100 (moves args)
+
+test_parseSlidingPuzzleTrailingArgs :: TestTree
+test_parseSlidingPuzzleTrailingArgs =
+  let args = parseSlidingPuzzleArgs ["100", "dummy"]
+   in testCase "Testing sliding puzzle trailing args" $ do
+        errored <-
+          try (evaluate args) :: IO (Either SomeException SlidingPuzzleArgs)
+        case errored of
+          Right _ -> failedSlidingPuzzleTrailingArgs
+          Left _  -> assertSomeException
+
+assertSomeException :: IO ()
+assertSomeException = pure ()
+
+failedSlidingPuzzleTrailingArgs :: IO ()
+failedSlidingPuzzleTrailingArgs =
+  assertFailure "Did not catch trailing args error"

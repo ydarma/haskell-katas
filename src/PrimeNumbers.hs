c3a74bfdc3e0abeeec5a0812fa7aa9adbe4d6c43
ydarma <yves.darmaillac@gmail.com>
2018-11-03 23:07:50 +0100

    Odd prime generator completed

--- src/PrimeNumbers.hs
+++ src/PrimeNumbers.hs
@@ -1,11 +1,30 @@
 module PrimeNumbers where
 
-data OddPrimeGenerator = OddPrimeGenerator { primes :: [Int], multiplesOfPrimes :: [Int] }
+data OddPrimeGenerator = OddPrimeGenerator
+  { primes            :: [Int]
+  , multiplesOfPrimes :: [Int]
+  }
 
 makeOddPrimeGenerator :: OddPrimeGenerator
-makeOddPrimeGenerator = OddPrimeGenerator { primes = [3], multiplesOfPrimes = [] }
+makeOddPrimeGenerator = OddPrimeGenerator {primes = [3], multiplesOfPrimes = []}
 
-isSquareOfNextRelevantPrime :: OddPrimeGenerator -> Int -> (OddPrimeGenerator, Bool)
+generatePrimesUntil :: Int -> OddPrimeGenerator
+generatePrimesUntil upper =
+  let accumulate generator candidate = fst (isPrime generator candidate)
+   in foldl accumulate makeOddPrimeGenerator [5, 7..upper]
+
+isPrime :: OddPrimeGenerator -> Int -> (OddPrimeGenerator, Bool)
+isPrime generator candidate =
+  let (squareGenerator, isSquare) =
+        isSquareOfNextRelevantPrime generator candidate
+      (multipleGenerator, isMultiple) =
+        isMultipleOfPrime squareGenerator candidate
+   in if isSquare
+        then (squareGenerator, False)
+        else (multipleGenerator, not isMultiple)
+
+isSquareOfNextRelevantPrime ::
+     OddPrimeGenerator -> Int -> (OddPrimeGenerator, Bool)
 isSquareOfNextRelevantPrime generator candidate =
   let nextRelevant = primes generator !! (length $ multiplesOfPrimes generator)
       nextSquare = nextRelevant * nextRelevant
@@ -17,31 +36,44 @@ isSquareOfNextRelevantPrime generator candidate =
 
 isMultipleOfPrime :: OddPrimeGenerator -> Int -> (OddPrimeGenerator, Bool)
 isMultipleOfPrime generator candidate =
-  let incr multiple = if multiple < candidate
-                        then multiple
-                        else incr (multiple + 2 * candidate)
-      check [] = False
-      check (base:others) =
-         let multiple = incr base
+  let multiplesAndPrimes = zip (multiplesOfPrimes generator) (primes generator)
+      (isMultiple, multiples) =
+        updateMultiplesForCandidate candidate multiplesAndPrimes
+      generatorIfPrime = addPrime generator candidate
+      generatorUpdate
+        | isMultiple = setMultiples generator multiples
+        | otherwise = setMultiples generatorIfPrime multiples
+   in (generatorUpdate, isMultiple)
+
+updateMultiplesForCandidate :: Int -> [(Int, Int)] -> (Bool, [Int])
+updateMultiplesForCandidate candidate multiplesAndPrimes =
+  let incr (multiple, prime) =
+        if multiple < candidate
+          then incr ((multiple + 2 * prime), prime)
+          else multiple
+      update [] = (False, [])
+      update (next:others) =
+        let multiple = incr next
+            (isOtherMultiple, otherMultiples) = update others
          in if multiple == candidate
-           then True
-           else check others
-      isMultiple = check (primes generator)
-      newGenerator
-        | isMultiple = generator
-        | otherwise = addPrime generator candidate
-   in (newGenerator, isMultiple)
+              then (True, multiple : map fst others)
+              else (isOtherMultiple, multiple : otherMultiples)
+   in update multiplesAndPrimes
 
 addMultiple :: OddPrimeGenerator -> Int -> OddPrimeGenerator
 addMultiple generator multiple =
-  OddPrimeGenerator {
-    primes = primes generator,
-    multiplesOfPrimes = multiplesOfPrimes generator ++ [multiple]
-  }
+  OddPrimeGenerator
+    { primes = primes generator
+    , multiplesOfPrimes = multiplesOfPrimes generator ++ [multiple]
+    }
 
 addPrime :: OddPrimeGenerator -> Int -> OddPrimeGenerator
 addPrime generator prime =
-  OddPrimeGenerator {
-    primes = primes generator ++ [prime],
-    multiplesOfPrimes = multiplesOfPrimes generator
-  }
+  OddPrimeGenerator
+    { primes = primes generator ++ [prime]
+    , multiplesOfPrimes = multiplesOfPrimes generator
+    }
+
+setMultiples :: OddPrimeGenerator -> [Int] -> OddPrimeGenerator
+setMultiples generator multiples =
+  OddPrimeGenerator {primes = primes generator, multiplesOfPrimes = multiples}
--- test/PrimeNumbersSpec.hs
+++ test/PrimeNumbersSpec.hs
@@ -1,8 +1,8 @@
 module PrimeNumbersSpec where
 
-import PrimeNumbers
-import Test.Tasty
-import Test.Tasty.HUnit
+import           PrimeNumbers
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
 test_build :: TestTree
 test_build =
@@ -25,7 +25,10 @@ test_addPrime =
       generator5 = addPrime generator 5
    in testCase "Testing add prime" $
       assertEqual "Primes are [3, 5]" [3, 5] (primes generator5) >>
-      assertEqual "Multiples are the same" (multiplesOfPrimes generator) (multiplesOfPrimes generator5)
+      assertEqual
+        "Multiples are the same"
+        (multiplesOfPrimes generator)
+        (multiplesOfPrimes generator5)
 
 test_isSquareOfNextRelevantPrime :: TestTree
 test_isSquareOfNextRelevantPrime =
@@ -45,3 +48,50 @@ test_isMultipleOfPrime =
    in testCase "Testing multiple of prime" $
       assertBool "5 is not multiple of prime" (not isMultiple5) >>
       assertEqual "Primes are [3, 5]" [3, 5] (primes generator5)
+
+test_multipleOfPrimesIncrement :: TestTree
+test_multipleOfPrimesIncrement =
+  let generator = makeOddPrimeGenerator
+      generator5 = addPrime generator 5
+      generator7 = addPrime generator5 7
+      generator9 = addMultiple generator7 9
+      (generator11, isMultiple11) = isMultipleOfPrime generator9 11
+   in testCase "Testing multiple increment" $
+      assertBool "11 is not multiple of prime" (not isMultiple11) >>
+      assertEqual "Multiples is [15]" [15] (multiplesOfPrimes generator11)
+
+test_isPrime :: TestTree
+test_isPrime =
+  let generator = makeOddPrimeGenerator
+      (generator5, isPrime5) = isPrime generator 5
+      (generator7, isPrime7) = isPrime generator5 7
+      (generator9, isPrime9) = isPrime generator7 9
+      (generator11, isPrime11) = isPrime generator9 11
+      (generator13, isPrime13) = isPrime generator11 13
+      (generator15, isPrime15) = isPrime generator13 15
+   in testCase "Testing prime test" $
+      assertEqual "Primes are [3, 5, 7, 11]" [3, 5, 7, 11] (primes generator11) >>
+      assertEqual "Multiples is [15]" [15] (multiplesOfPrimes generator11) >>
+      assertEqual "Primes are [3, 5, 7, 11, 13]" [3, 5, 7, 11, 13] (primes generator13) >>
+      assertEqual "Multiples is [15]" [15] (multiplesOfPrimes generator13) >>
+      assertEqual "Primes are [3, 5, 7, 11, 13]" [3, 5, 7, 11, 13] (primes generator15) >>
+      assertEqual "Multiples is [15]" [15] (multiplesOfPrimes generator15)
+
+test_generateUntil :: TestTree
+test_generateUntil =
+  let generator = generatePrimesUntil 30
+   in testCase "Testing prime generation" $
+      assertEqual
+        "Primes are [3, 5, 7, 11, 13, 17, 19, 23, 29]"
+        [3, 5, 7, 11, 13, 17, 19, 23, 29]
+        (primes generator) >>
+      assertEqual "Multiples are [33, 35]" [33, 35] (multiplesOfPrimes generator)
+
+test_notGreaterThan :: TestTree
+test_notGreaterThan =
+  let generator157 = generatePrimesUntil 157
+      generator162 = generatePrimesUntil 162
+   in testCase "Testing 157 is prime" $
+      assertEqual "157 is prime" 157 (last $ primes generator157) >>
+      assertEqual "157 is 36th odd prime" 36 (length $ primes generator157) >>
+      assertEqual "No prime between 158 and 162" 157 (last $ primes generator162)
